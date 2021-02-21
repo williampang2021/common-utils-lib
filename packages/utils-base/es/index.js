ea6e21f24d7d671dc3c7b6bebf2d9baab1274256
williampang <williampang@tencent.com>
2021-02-21 10:49:39 +0800

    fix:优化打包

---
+++ packages/utils-base/es/index.js
@@ -0,0 +1,24 @@
+var getA = function getA() {
+  window.alert('aaaaaaaa11111');
+};
+var getB = function getB() {
+  window.alert('bbbbbbbbbb');
+};
+var getC = function getC() {
+  window.alert('cccccccc');
+};
+
+var getD = function getD() {
+  window.alert('dddddddd');
+};
+
+var getE = function getE() {
+  window.alert('eeeeeee');
+};
+
+var index = {
+  test: 1
+};
+
+export default index;
+export { getA, getB, getC, getD, getE };
--- packages/utils-base/src/index.js
+++ packages/utils-base/src/index.js
@@ -3,3 +3,7 @@ export { getA, getB, getC } from './utils'
 export { getD } from './getD'
 
 export { default as getE } from './getE'
+
+export default {
+  test: 1
+}
