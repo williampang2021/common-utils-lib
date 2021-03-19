91ea44827802604375a398c74aa086c1963c3432
williampang <1119164688@qq.com>
2021-03-20 02:21:09 +0800

    fix: 增加docs

--- packages/base-utils/src/utils.ts
+++ packages/base-utils/src/utils.ts
@@ -1,4 +1,14 @@
+/**
+* 获取A的值
+* @param name 姓名
+* @param age 年龄
+*/
 
+// todo  配置写到tsconfig/json中
+// 使用webpack插件 //https://www.npmjs.com/package/typedoc-webpack-plugin
+// typedoc-plugin-single-line-tags
+// typedoc-plugin-external-module-name
+// typedoc-plugin-toc-group
 export const getA = (message:string='tst') => {
   window.alert('aaaaaaaa11111'+message)
 }
@@ -7,6 +17,6 @@ export const getB = () => {
   window.alert('bbbbbbbbbb')
 }
 
-export const getC = () => {
+export function getC () {
   window.alert('cccccccc')
 }
